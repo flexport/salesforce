fc21441088e6a93c9d261697682d77a5c657fb98
Art Peel <apeel@flexport.com>
2019-06-20 14:35:53 -0700

    update README and bump version to 2.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.flexport-clojure-eng/salesforce "1.0.5"
+(defproject org.clojars.flexport-clojure-eng/salesforce "2.0.1-SNAPSHOT"
   :description "A clojure library for accessing the salesforce api"
   :url "https://www.flexport.com"
   :license {:name "Eclipse Public License"
